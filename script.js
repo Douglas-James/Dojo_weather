8b0bfa965201d007b04d663cd1dca351f0580d04
Douglas-James <ryle@drumclog.com>
2022-05-30 08:59:50 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function weather()
+{
+  alert('Loading weather report..')
+}
+
+function city()
+{
+  alert("city map loading..")
+}
+
+
+function accept()
+{
+  var cook = document.querySelector(".cook");
+  cook.remove();
+}
